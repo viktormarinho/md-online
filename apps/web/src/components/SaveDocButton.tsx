5f64bed2df22f07c402bda79fad74d0ee2eeff7c
viktormarinho <viktormpcs@gmail.com>
2022-11-15 00:17:50 -0300

    Save document button working - Ctrl + S too (Cleaned up all code)

---
+++ apps/web/src/components/SaveDocButton.tsx
@@ -0,0 +1,48 @@
+import { useCallback, useEffect } from "react";
+import { useDocStore } from "../state/documentState";
+import { trpc } from "../utils/trpc";
+
+
+export function SaveDocButton({ id }: { id: string }) {
+    const text = useDocStore(state => state.currentDocText);
+    const [isSaved, setIsSaved] = useDocStore(state => ([state.isSaved, state.setIsSaved]));
+    const saveDocMutation = trpc.updateDoc.useMutation({
+        onSuccess: ({ doc }) => {
+            setIsSaved(doc.content === text);
+        }
+    })
+
+    const saveDoc = useCallback(() => {
+        saveDocMutation.mutate({
+            id,
+            newContent: text
+        })
+    }, [id, saveDocMutation, text])
+    
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (event.ctrlKey && event.key == 's' && !event.repeat) {
+                event.preventDefault();
+                event.stopPropagation();
+                saveDoc();
+            }
+        }
+        
+        document.addEventListener('keydown', handleKeyPress);
+        
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [saveDoc])
+
+    return (
+        <button
+            onClick={saveDoc}
+            disabled={isSaved} 
+            className="mt-2 rounded-lg shadow-lg border py-1 px-2 disabled:opacity-60"
+            title="Press save or Ctrl + S"
+        >
+            {isSaved ? 'All changes saved' : 'Save'}
+        </button>
+    )
+}
