65d47766c94df50340c8a4dac05b8d2a9f1aca70
viktormarinho <viktormpcs@gmail.com>
2022-11-15 00:17:06 -0300

    Index is the doclist page now

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,8 @@
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import { Doclist } from "../components/Doclist";
+import { Navbar } from "../components/Navbar";
 import { Spinner } from "../components/Spinner";
 import { User, useSession } from "../utils/session";
-import { trpc } from "../utils/trpc";
 
 
 export default function DocsRoute() {
@@ -18,28 +17,14 @@ export default function DocsRoute() {
         return <div className="w-full h-full flex justify-center items-center"><Spinner /></div>
     }
 
-    return (
-        <DocsPage user={data.user}/>
-    )
+    return <DocsPage user={data.user}/>;
 }
 
 function DocsPage({ user }: { user: User | undefined}) {
-    const navigate = useNavigate();
-    const logoutMutation = trpc.endSession.useMutation({
-        onSuccess: ({ msg }) => {
-            toast(msg, { type: 'success', position: 'top-center' });
-            localStorage.removeItem('sessionId');
-            navigate('/login');
-        }
-    })
-
-    const logout = () => {
-        logoutMutation.mutate();
-    }
 
     return (
         <div>
-            <h1>Welcome, {user?.username} - <button onClick={logout}>Logout</button></h1>
+            <Navbar user={user} />
             <Doclist />
         </div>
     )
